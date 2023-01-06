cc52b3a634a21358cf2a75b0361071f1efc29df6
slam-xu <xuyuxuan666@outlook.com>
2023-01-06 14:08:23 +0100

    xu

--- ch2/helloworld.cpp
+++ ch2/helloworld.cpp
@@ -3,6 +3,8 @@ using namespace std;
 
 int main( int argc, char** argv )
 {
+    cout<<"============ S L A M ============"<<endl;
+    cout<<"============ X U A N ============"<<endl;
     cout<<"Hello SLAM! SLAM"<<endl;
     return 0;
 }
